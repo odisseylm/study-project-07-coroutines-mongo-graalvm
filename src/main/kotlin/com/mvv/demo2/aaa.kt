097ccdebfe48e34c5bb0bf7af0a1befc304309fc
vmelnykov <Volodymyr.Melnykov@gmail.com>
2024-02-19 03:15:06 +0200

    Using reactive mongo connection.

--- src/main/kotlin/com/mvv/demo2/aaa.kt
+++ src/main/kotlin/com/mvv/demo2/aaa.kt
@@ -2,13 +2,18 @@ package com.mvv.demo2
 
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoCollection
+import com.mongodb.reactivestreams.client.MongoCollection as RMongoCollection
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import org.bson.Document
 import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate as RMongoTemplate
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 
 data class Customer (
@@ -24,6 +29,7 @@ data class Banner (
 //@PathVariable("")
 /*open*/ class MyController(
     private val mongoOperations: MongoOperations,
+    private val rMongoOperations: ReactiveMongoOperations,
 ) {
 
     @GetMapping("/customers")
@@ -51,6 +57,53 @@ data class Banner (
         val allUsers: List<Document> = allUsersIt1.asIterable().toList()
 
         return listOf(Customer("all our users count ${allUsers.size}"))
+    }
+
+    @GetMapping("/test-rx")
+    @Suppress("UNUSED_VARIABLE", "ReactiveStreamsUnusedPublisher")
+    fun testRx():
+            //Mono<List<Customer>>
+            Flux<Customer>
+    {
+
+        val users: Mono<RMongoCollection<Document>> = rMongoOperations.getCollection("users")
+
+        if (rMongoOperations is RMongoTemplate) {
+            val mongoDatabaseFactory = rMongoOperations.mongoDatabaseFactory
+            val db = mongoDatabaseFactory.mongoDatabase
+            println("mongo db: $db")
+        }
+
+        val exampleOfUsageCount01: Mono<List<Customer>> = users
+            .flatMapMany { it.countDocuments() }
+            .take(1)
+            .single()
+            //.doOnEach { userCount -> println("## user count $userCount") } // TODO: It is something strange??? How to use it?
+            .doOnNext { userCount -> println("## user count $userCount") }
+            .map { allUsersCount -> Customer("all our users count $allUsersCount") }
+            .map { listOf(it) }
+
+        val exampleOfUsageCount02: Flux<Customer> = users
+            .flatMapMany { it.countDocuments() }
+            .take(1)
+            //.doOnEach { userCount -> println("## user count $userCount") } // TODO: It is something strange??? How to use it?
+            .doOnNext { userCount -> println("## user count $userCount") }
+            .map { allUsersCount -> Customer("all our users count $allUsersCount") }
+            .map { it }
+
+        val userEntities: Flux<Customer> = users
+            .flatMapMany { it.find() }
+            .doOnNext { doc -> println("## user $doc ( ${doc["name"]} )") }
+            .map { userDoc -> Customer(userDoc.getString("name")) }
+            .map { it }
+
+        return userEntities
+    }
+
+    @GetMapping("/test-coroutine")
+    suspend fun testCoroutine(): List<Customer> {
+        //val users: Mono<RMongoCollection<Document>> = rMongoOperations.getCollection("users")
+        TODO()
     }
 
     //@GetMapping("/test-temp2")

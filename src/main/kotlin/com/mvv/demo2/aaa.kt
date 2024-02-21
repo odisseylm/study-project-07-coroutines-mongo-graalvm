6bc405c2ca9cff8fcace4f6179571afa9e74f2e1
vmelnykov <Volodymyr.Melnykov@gmail.com>
2024-02-21 04:31:01 +0200

    Dumping current threads

--- src/main/kotlin/com/mvv/demo2/aaa.kt
+++ src/main/kotlin/com/mvv/demo2/aaa.kt
@@ -2,29 +2,29 @@ package com.mvv.demo2
 
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoCollection
-import com.mongodb.reactivestreams.client.MongoCollection as RMongoCollection
 import kotlinx.coroutines.delay
-import org.bson.Document
-import org.springframework.data.mongodb.core.MongoOperations
-import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate as RMongoTemplate
-import org.springframework.data.mongodb.core.ReactiveMongoOperations
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-
-import com.mongodb.kotlin.client.coroutine.MongoClient as CorMongoClient
-import kotlinx.coroutines.flow.Flow as CorFlow
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import org.apache.commons.lang3.ThreadUtils
+import org.bson.Document
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import com.mongodb.kotlin.client.coroutine.MongoClient as CorMongoClient
+import com.mongodb.reactivestreams.client.MongoCollection as RMongoCollection
+import kotlinx.coroutines.flow.Flow as CorFlow
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate as RMongoTemplate
 
 
 data class Customer (
@@ -87,7 +87,11 @@ class MyController(
             Flux<Customer>
     {
 
+        println("## test-rx 01, thread: $currentThreadName")
+
         val users: Mono<RMongoCollection<Document>> = rMongoOperations.getCollection("users")
+
+        println("## test-rx 02, thread: $currentThreadName")
 
         if (rMongoOperations is RMongoTemplate) {
             val mongoDatabaseFactory = rMongoOperations.mongoDatabaseFactory
@@ -96,13 +100,23 @@ class MyController(
         }
 
         val exampleOfUsageCount01: Mono<List<Customer>> = users
-            .flatMapMany { it.countDocuments() }
+            .flatMapMany {
+                println("## test-rx 10, thread: $currentThreadName")
+                it.countDocuments()
+            }
             .take(1)
             .single()
             //.doOnEach { userCount -> println("## user count $userCount") } // TODO: It is something strange??? How to use it?
-            .doOnNext { userCount -> println("## user count $userCount") }
-            .map { allUsersCount -> Customer("all our users count $allUsersCount") }
-            .map { listOf(it) }
+            .doOnNext { userCount ->
+                println("## test-rx 11, thread: $currentThreadName")
+                println("## user count $userCount") }
+            .map { allUsersCount ->
+                println("## test-rx 12, thread: $currentThreadName")
+                Customer("all our users count $allUsersCount") }
+            .map {
+                println("## test-rx 13, thread: $currentThreadName")
+                //dumpThreadNames()
+                listOf(it) }
 
         val exampleOfUsageCount02: Flux<Customer> = users
             .flatMapMany { it.countDocuments() }
@@ -113,9 +127,16 @@ class MyController(
             .map { it }
 
         val userEntities: Flux<Customer> = users
-            .flatMapMany { it.find() }
-            .doOnNext { doc -> println("## user $doc ( ${doc["name"]} )") }
-            .map { userDoc -> Customer(userDoc.getString("name")) }
+            .flatMapMany {
+                println("## test-rx 30, thread: $currentThreadName")
+                it.find() }
+            .doOnNext { doc ->
+                println("## test-rx 31, thread: $currentThreadName")
+                println("## user $doc ( ${doc["name"]} )") }
+            .map { userDoc ->
+                println("## test-rx 32, thread: $currentThreadName")
+                //dumpThreadNames()
+                Customer(userDoc.getString("name")) }
 
         return userEntities
     }
@@ -131,11 +152,23 @@ class MyController(
         //    .doOnNext { doc -> println("## user $doc ( ${doc["name"]} )") }
         //    .map { userDoc -> Customer(userDoc.getString("name")) }
 
+        println("## test-rx2 01, thread: $currentThreadName")
+
         val users: Flux<User> = rxUserRepository.findByName("user1")
 
+        println("## test-rx2 02, thread: $currentThreadName")
+
         val userEntities: Flux<Customer> = users
-            .doOnNext { user -> println("## user $user ( ${user._id}/${user.name} )") }
-            .map { user -> Customer(user.name) }
+            .doOnNext { user ->
+                println("## test-rx2 10, thread: $currentThreadName")
+                println("## user $user ( ${user._id}/${user.name} )") }
+            .map { user ->
+                println("## test-rx2 11, thread: $currentThreadName")
+                //dumpThreadNames()
+                dumpThreadNames()
+                Customer(user.name) }
+
+        println("## test-rx2 03, thread: $currentThreadName")
 
         return userEntities
     }
@@ -143,13 +176,26 @@ class MyController(
     @GetMapping("/test-coroutine")
     suspend fun testCoroutine(): CorFlow<Customer> {
 
+        println("## test-coroutine 01, thread: $currentThreadName")
+
         val database = crMongoClient.getDatabase("db1")
 
-        val users = database.getCollection<Document>("users")
+        println("## test-coroutine 02, thread: $currentThreadName")
+
+        val users: com.mongodb.kotlin.client.coroutine.MongoCollection<Document> = database.getCollection<Document>("users")
+
+        println("## test-coroutine 03, thread: $currentThreadName")
 
         val res: CorFlow<Customer> = users.find()
-            .onEach { doc -> println("## user $doc ( ${doc["name"]} )") }
-            .map { userDoc -> Customer(userDoc.getString("name")) }
+            .onEach { doc ->
+                println("## test-coroutine 10, thread: $currentThreadName")
+                println("## user $doc ( ${doc["name"]} )") }
+            .map { userDoc ->
+                println("## test-coroutine 11, thread: $currentThreadName")
+                //dumpThreadNames()
+                Customer(userDoc.getString("name")) }
+
+        println("## test-coroutine 04, thread: $currentThreadName")
 
         return res
     }
@@ -157,11 +203,22 @@ class MyController(
     @GetMapping("/test-coroutine2")
     suspend fun testCoroutine2(): CorFlow<Customer> {
 
+        println("## test-coroutine2 01, thread: $currentThreadName")
+
         val users: CorFlow<User> = this.corUserRepository.findByName("user2")
 
+        println("## test-coroutine2 02, thread: $currentThreadName")
+
         val res: CorFlow<Customer> = users
-            .onEach { doc -> println("## user $doc ( ${doc._id} / ${doc.name} )") }
-            .map { user -> Customer(user.name) }
+            .onEach { doc ->
+                println("## test-coroutine2 10, thread: $currentThreadName")
+                println("## user $doc ( ${doc._id} / ${doc.name} )") }
+            .map { user ->
+                println("## test-coroutine2 11, thread: $currentThreadName")
+                //dumpThreadNames()
+                Customer(user.name) }
+
+        println("## test-coroutine2 03, thread: $currentThreadName")
 
         return res
     }
@@ -169,29 +226,57 @@ class MyController(
     @GetMapping("/test-coroutine3")
     suspend fun testCoroutine3(): List<Customer> {
 
+        println("## test-coroutine3 01, thread: $currentThreadName")
+
         val users: List<User> = this.corUserRepository.findAllByName("user2")
 
+        println("## test-coroutine3 02, thread: $currentThreadName")
+
         val res: List<Customer> = users
-            .onEach { doc -> println("## user $doc ( ${doc._id} / ${doc.name} )") }
-            .map { user -> Customer(user.name) }
+            .onEach { doc ->
+                println("## test-coroutine3 10, thread: $currentThreadName")
+                println("## user $doc ( ${doc._id} / ${doc.name} )") }
+            .map { user ->
+                println("## test-coroutine3 11, thread: $currentThreadName")
+                //dumpThreadNames()
+                Customer(user.name) }
+
+        println("## test-coroutine3 03, thread: $currentThreadName")
+
+        //dumpThreadNames()
 
         return res
     }
 
     @GetMapping("/test-coroutine4")
     suspend fun testCoroutine4(): List<Customer> {
+
+        println("## test-coroutine4 01, thread: $currentThreadName")
+
         val user: User? = this.corUserRepository.findUserByName("user2") // "user2_unknown")
+
+        println("## test-coroutine4 02, thread: $currentThreadName")
+        //dumpThreadNames()
+
         val res = if (user != null) listOf(Customer(user.name)) else emptyList()
         return res
     }
 
     @GetMapping("/test-coroutine5")
     suspend fun testCoroutine5(): List<Customer> {
+
+        println("## test-coroutine5 01, thread: $currentThreadName")
+
         val user: User = this.corUserRepository.getByName("user2") //"user2_unknown")
+
+        println("## test-coroutine5 02, thread: $currentThreadName")
 
         //corUserRepository.findOne(user._id)
         corUserRepository.findById(user._id)
             ?: throw IllegalStateException("findOne does not work.")
+
+        println("## test-coroutine5 03, thread: $currentThreadName")
+        //dumpThreadNames()
 
         val res = listOf(Customer(user.name))
         return res
@@ -205,7 +290,7 @@ class MyController(
 
     @GetMapping("/flow")
     fun flowEndpoint() = flow {
-        val banner = Banner("fuck")
+        val banner = Banner("banner1")
         delay(10)
         emit(banner)
         delay(10)
@@ -213,6 +298,7 @@ class MyController(
     }
 }
 
+private val currentThreadName: String get() = Thread.currentThread().name
 
 @org.springframework.data.mongodb.core.mapping.Document("users")
 data class User (
@@ -250,4 +336,13 @@ interface CoroutineUserRepository : CoroutineCrudRepository<User, ObjectId> {
     // It has warnings, but it works ok and for SMALL collections it should be less/more ok (in my opinion).
     @Suppress("SpringDataRepositoryMethodReturnTypeInspection")
     suspend fun findAllByName(id: String): List<User>
+}
+
+private fun dumpThreadNames() {
+    //val threads: Set<Thread> = Thread.getAllStackTraces().keys
+    val threads: Collection<Thread> = ThreadUtils.getAllThreads()
+    val asStr = threads
+        .sortedBy { it.name }
+        .joinToString("\n") { "  ${it.name}" }
+    println("## All threads:\n$asStr\n")
 }
